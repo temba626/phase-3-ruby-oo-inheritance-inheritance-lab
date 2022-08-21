54e8985b1d8bc33ad7b35672cae24ac6aa35a045
Brandon Temba <brandontemba560@gmail.com>
2022-08-21 13:00:41 +0300

    Done.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,19 +1,19 @@
-describe Student do 
+describe Student do
   let!(:student) { Student.new }
 
   it "inherits from the User class" do
-    expect(Student.superclass).to be(User) 
+    expect(Student.superclass).to be(User)
   end
-  
-  describe "#first_name" do 
-    it "has a first name" do 
+
+  describe "#first_name" do
+    it "has a first name" do
       student.first_name = "Steve"
       expect(student.first_name).to eq("Steve")
     end
   end
 
-  describe "#last_name" do 
-    it "has a last name" do 
+  describe "#last_name" do
+    it "has a last name" do
       student.last_name = "Jobs"
       expect(student.last_name).to eq("Jobs")
     end
@@ -22,20 +22,20 @@ describe Student do
   describe ".new" do
     it "is initialized with an empty @knowledge array" do
       sophie = Student.new
-      expect(sophie.instance_variable_get(:@knowledge)).to eq([]) 
-    end 
+      expect(sophie.instance_variable_get(:@knowledge)).to eq([])
+    end
   end
 
-  describe "#learn" do 
-    it "takes in an argument of a string of knowledge and adds it to the student's knowledge array" do 
+  describe "#learn" do
+    it "takes in an argument of a string of knowledge and adds it to the student's knowledge array" do
       student.learn("Ruby framework Rails gem bundle update")
       expect(student.instance_variable_get(:@knowledge)).to include("Ruby framework Rails gem bundle update")
     end
   end
 
-  describe "#knowledge" do 
-    it "returns that student's knowledge array" do 
-      student.learn("Javascript Ember Elixir knowledge") 
+  describe "#knowledge" do
+    it "returns that student's knowledge array" do
+      student.learn("Javascript Ember Elixir knowledge")
       expect(student.knowledge).to include("Javascript Ember Elixir knowledge")
     end
   end
